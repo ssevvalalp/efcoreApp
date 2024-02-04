a6f9f839dc67a8ab0525d5cdaed9f82bb8964a17
Şevval Alp <ssevvalalp@gmail.com>
2024-02-04 23:37:46 +0300

    Student Controller, Student/Create View created

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,28 @@
+﻿using efcoreApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace efcoreApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly DataContext _context;
+        public StudentController(DataContext context)
+        {
+
+            _context = context; 
+
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task <IActionResult> Create(Student model)
+        {
+            _context.Students.Add(model);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index","Home");
+            return View();
+        }
+    }
+}
