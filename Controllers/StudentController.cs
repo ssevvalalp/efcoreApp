d802930ba17c81aac990b69196879fa6289e4e1b
Şevval Alp <ssevvalalp@gmail.com>
2024-02-05 12:31:47 +0300

    Registration List

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,10 +1,13 @@
 ﻿using efcoreApp.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace efcoreApp.Controllers
 {
     public class StudentController : Controller
     {
+        //------ injection
+       // var context = new DataContext();
         private readonly DataContext _context;
         public StudentController(DataContext context)
         {
@@ -12,17 +15,26 @@ namespace efcoreApp.Controllers
             _context = context; 
 
         }
+        //------
+
+
+        public async Task <IActionResult> Index()
+        {
+            //var students = _context.Students.ToList(); //senkron
+            var students =await  _context.Students.ToListAsync();
+            return View(students);
+        }
         public IActionResult Create()
         {
             return View();
         }
         [HttpPost]
-        public async Task <IActionResult> Create(Student model)
+        public async Task <IActionResult> Create(Student model)  //database iletişimlerinde async. yemek sipariş örneği
         {
             _context.Students.Add(model);
             await _context.SaveChangesAsync();
-            return RedirectToAction("Index","Home");
-            return View();
+            return RedirectToAction("Index");
+            
         }
     }
 }
--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -4,13 +4,14 @@ namespace efcoreApp.Data
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
         public DbSet<Course> Courses => Set<Course>(); //initilized/ = null!;
         public DbSet<Student> Students => Set<Student>();
         public DbSet<CourseRegistration> CourseRegistrations => Set<CourseRegistration>();
 
-        public DataContext(DbContextOptions<DataContext> options) : base(options)
-        {
-        }
+      
 
 
     }
